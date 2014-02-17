dd5d671cbe94dd9a698a23edf0e88b10dea5ae23
He Yongli <yongli.he@intel.com>
2014-02-17 13:49:01 +0800

    Use six.StringIO/BytesIO instead of StringIO.StringIO
    to keep Python 3.x compatibility, use six.StringIO/BytesIO to
    replace StringIO.StringIO
    
    StringIO works for unicode
    BytesIO works for bytes
    
    Change-Id: Iffd3cf97c1c17844fef083148c090473e1dd5816
    Closes-Bug: #1280100

--- gantt/tests/scheduler/test_scheduler_options.py
+++ gantt/tests/scheduler/test_scheduler_options.py
@@ -17,9 +17,9 @@ Tests For PickledScheduler.
 """
 
 import datetime
-import StringIO
 
 from nova.openstack.common import jsonutils
+import six
 
 from gantt.scheduler import scheduler_options
 from gantt import test
@@ -45,7 +45,7 @@ class FakeSchedulerOptions(scheduler_options.SchedulerOptions):
 
     def _get_file_handle(self, filename):
         self.file_was_loaded = True
-        return StringIO.StringIO(self._file_data)
+        return six.StringIO(self._file_data)
 
     def _get_time_now(self):
         return self._time_now
