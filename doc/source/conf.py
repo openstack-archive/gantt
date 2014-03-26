fa3cbf93da8977834f88ca06ed288577215e5028
Sergey Lukjanov <slukjanov@mirantis.com>
2014-03-26 15:26:33 +0400

    Switch to oslosphinx
    It was renamed.
    
    Change-Id: Ibfbcda80235998c1a0f8516483ddaff342f72450
    Closes-Bug: #1277168

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -35,7 +35,7 @@ extensions = ['sphinx.ext.autodoc',
               'sphinx.ext.pngmath',
               'sphinx.ext.ifconfig',
               'sphinx.ext.graphviz',
-              'oslo.sphinx',
+              'oslosphinx',
               ]
 
 todo_include_todos = True
